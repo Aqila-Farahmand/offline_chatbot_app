08fd9da1e5310dd33456aafb528cb5fd4b8069d4
Aqila Farahmand <aqela.af@gmail.com>
2025-07-10 10:36:26 +0200

    refactor: modify the app name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:medicoai/main.dart';
+import 'package:medico_ai/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
