30ceefb48275395848ecf1747457d59e45c56bd8
Aqila Farahmand <aqela.af@gmail.com>
2025-07-25 10:54:01 +0200

    fix: configure the app module

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,15 +1,28 @@
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
+    // Add the dependency for the Google services Gradle plugin
     id("com.google.gms.google-services")
     // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+    // enable Material Components
+    implementation("com.google.android.material:material:<version>")
 
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+}
 android {
-    namespace = "com.example.medicoai"
+    namespace = "it.aqila.farahmand.medicoai"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -23,14 +36,14 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.medicoai"
+        // Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "it.aqila.farahmand.medicoai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = 1.0
     }
 
     buildTypes {
